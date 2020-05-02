41316dff52218ed02e4932fa0af21f90ccc19bc2
bbiswal <bidhubhusan.biswal@valeo.com>
2020-05-02 21:06:50 +0530

    Seeting first page as login page

--- Zip-A-Zap/Zip-A-Zap/App_Start/RouteConfig.cs
+++ Zip-A-Zap/Zip-A-Zap/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Zip_A_Zap
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
