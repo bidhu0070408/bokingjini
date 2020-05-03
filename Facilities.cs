20cb5d9fba3bbe263dcca28d5307fe9250be6f9d
bidhu0070408 <61492548+bidhu0070408@users.noreply.github.com>
2020-05-03 15:18:27 +0530

    Add files via upload

---
+++ Facilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zip_A_Zap.Models
+{
+    public class Facilities
+    {
+
+        public string UserName { get; set; }
+
+        public string serviceSubject { get; set; }
+        public string userText { get; set; }
+
+
+        public string assignedTo { get; set; }
+
+        public DateTime fixedTIme { get; set; }
+    }
+         
+}
---
+++ Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zip_A_Zap.Models
+{
+    public class Room
+    {
+
+        public string Username { get; set; }
+        public string location { get; set; }
+        public int RoomNumber { get; set; }
+        public string RoomType { get; set; }
+
+        public int PersonsPrrRoom { get; set; }
+        public DateTime CheckIn{ get; set; }
+    public DateTime CheckOut { get; set; }
+}
+    }
+}
